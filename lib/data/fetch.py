9acc3373c28e77e601dc09ef7b9e53ed6dd2772a
Achyudh K Ram <achyudhk@gmail.com>
2018-01-16 16:10:54 -0500

    Fetch reactions with comments

--- lib/data/fetch.py
+++ lib/data/fetch.py
@@ -11,6 +11,9 @@ http_auth_secret = client_config['HTTP_AUTH_SECRET']
 http_auth = HTTPBasicAuth(http_auth_username, http_auth_secret)
 app.config['SESSION_TYPE'] = 'mongodb'
 
+# Header to get reactions along with comments
+reactions_header = {'Accept': 'application/vnd.github.squirrel-girl-preview'}
+
 
 def rate_reset_wait(headers):
     if headers['X-RateLimit-Remaining'] <= 0:
@@ -21,8 +24,11 @@ def rate_reset_wait(headers):
         return "Positive RateLimit"
 
 
-def generic(request_url):
-    response_json = requests.get(request_url, auth=http_auth).json()
+def generic(request_url, headers=None):
+    if headers is not None:
+        response_json = requests.get(request_url, auth=http_auth, headers=headers).json()
+    else:
+        response_json = requests.get(request_url, auth=http_auth).json()
     wait_status = rate_reset_wait(response_json.headers)
     if wait_status == "Positive RateLimit":
         return response_json
@@ -48,25 +54,25 @@ def issues(full_repo_name):
 def commit_comments(full_repo_name, commit_ref=None):
     if commit_ref is None:
         request_url = 'https://api.github.com/repos/%s/comments' % full_repo_name
-        return generic(request_url)
+        return generic(request_url, headers=reactions_header)
     else:
         request_url = 'https://api.github.com/repos/%s/commits/%s/comments' % full_repo_name, commit_ref
-        return generic(request_url)
+        return generic(request_url, headers=reactions_header)
 
 
 def review_comments(full_repo_name, pr_number=None):
     if pr_number is None:
         request_url = 'https://api.github.com/repos/%s/pulls/comments' % full_repo_name
-        return generic(request_url)
+        return generic(request_url, headers=reactions_header)
     else:
         request_url = 'https://api.github.com/repos/%s/pulls/%s/comments' % full_repo_name, pr_number
-        return generic(request_url)
+        return generic(request_url, headers=reactions_header)
 
 
 def issue_comments(full_repo_name, issue_number=None):
     if issue_number is None:
         request_url = 'https://api.github.com/repos/%s/issues/comments' % full_repo_name
-        return generic(request_url)
+        return generic(request_url, headers=reactions_header)
     else:
         request_url = 'https://api.github.com/repos/%s/issues/%s/comments' % full_repo_name, issue_number
-        return generic(request_url)
+        return generic(request_url, headers=reactions_header)
