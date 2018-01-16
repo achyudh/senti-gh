bf58cba2b87998da9726e95521206355b40d358c
Achyudh K Ram <achyudhk@gmail.com>
2018-01-16 13:04:46 -0500

    Add full repo comment fetchers

--- lib/data/fetch.py
+++ lib/data/fetch.py
@@ -45,16 +45,28 @@ def issues(full_repo_name):
     return generic(request_url)
 
 
-def commit_comments(full_repo_name):
-    request_url = 'https://api.github.com/repos/%s/comments' % full_repo_name
-    return generic(request_url)
+def commit_comments(full_repo_name, commit_ref=None):
+    if commit_ref is None:
+        request_url = 'https://api.github.com/repos/%s/comments' % full_repo_name
+        return generic(request_url)
+    else:
+        request_url = 'https://api.github.com/repos/%s/commits/%s/comments' % full_repo_name, commit_ref
+        return generic(request_url)
 
 
-def review_comments(full_repo_name, issue_number):
-    request_url = 'https://api.github.com/repos/%s/issues/%s/comments' % full_repo_name, issue_number
-    return generic(request_url)
+def review_comments(full_repo_name, pr_number=None):
+    if pr_number is None:
+        request_url = 'https://api.github.com/repos/%s/pulls/comments' % full_repo_name
+        return generic(request_url)
+    else:
+        request_url = 'https://api.github.com/repos/%s/pulls/%s/comments' % full_repo_name, pr_number
+        return generic(request_url)
 
 
-def issue_comments(full_repo_name, issue_number):
-    request_url = 'https://api.github.com/repos/%s/issues/%s/comments' % full_repo_name, issue_number
-    return generic(request_url)
+def issue_comments(full_repo_name, issue_number=None):
+    if issue_number is None:
+        request_url = 'https://api.github.com/repos/%s/issues/comments' % full_repo_name
+        return generic(request_url)
+    else:
+        request_url = 'https://api.github.com/repos/%s/issues/%s/comments' % full_repo_name, issue_number
+        return generic(request_url)
