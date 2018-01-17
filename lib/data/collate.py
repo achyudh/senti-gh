b839a4d5a86aabc36693191d607b242407cdce30
Achyudh K Ram <achyudhk@gmail.com>
2018-01-17 16:51:54 -0500

    Fix TypeError in data.collate

--- lib/data/collate.py
+++ lib/data/collate.py
@@ -25,7 +25,7 @@ def by_user(full_repo_name):
     db.insert(repo_data, 'data/repo/%s.json' % full_repo_name)
     # Collate user data
     for issue in repo_data['issues']:
-        if 'pull_request' not in issue:
+        if 'pull_request' not in issue and issue['user'] is not None:
             if user_data.get(issue['user']['login'], None) is None:
                 init_user_data(user_data, issue['user']['login'])
             text_obj = dict()
@@ -46,44 +46,48 @@ def by_user(full_repo_name):
             user_data[commit['author']['login']]['commits'].append(text_obj)
 
     for pull_request in repo_data['pull_requests']:
-        if user_data.get(pull_request['user']['login'], None) is None:
-            init_user_data(user_data, pull_request['user']['login'])
-        text_obj = dict()
-        text_obj['url'] = pull_request['url']
-        text_obj['title'] = pull_request['title']
-        text_obj['body'] = pull_request['body']
-        text_obj['author_association'] = pull_request['author_association']
-        user_data[pull_request['user']['login']]['pull_requests'].append(text_obj)
+        if pull_request['user'] is not None:
+            if user_data.get(pull_request['user']['login'], None) is None:
+                init_user_data(user_data, pull_request['user']['login'])
+            text_obj = dict()
+            text_obj['url'] = pull_request['url']
+            text_obj['title'] = pull_request['title']
+            text_obj['body'] = pull_request['body']
+            text_obj['author_association'] = pull_request['author_association']
+            user_data[pull_request['user']['login']]['pull_requests'].append(text_obj)
 
     for issue_comment in repo_data['issue_comments']:
-        if user_data.get(issue_comment['user']['login'], None) is None:
-            init_user_data(user_data, issue_comment['user']['login'])
-        text_obj = dict()
-        text_obj['url'] = issue_comment['url']
-        text_obj['body'] = issue_comment['body']
-        text_obj['reactions'] = issue_comment['reactions']
-        text_obj['author_association'] = issue_comment['author_association']
-        user_data[issue_comment['user']['login']]['issue_comments'].append(text_obj)
+        if issue_comment['user'] is not None:
+            if user_data.get(issue_comment['user']['login'], None) is None:
+                init_user_data(user_data, issue_comment['user']['login'])
+            text_obj = dict()
+            text_obj['url'] = issue_comment['url']
+            text_obj['body'] = issue_comment['body']
+            text_obj['reactions'] = issue_comment['reactions']
+            text_obj['author_association'] = issue_comment['author_association']
+            user_data[issue_comment['user']['login']]['issue_comments'].append(text_obj)
 
     for commit_comment in repo_data['commit_comments']:
-        if user_data.get(commit_comment['user']['login'], None) is None:
-            init_user_data(user_data, commit_comment['user']['login'])
-        text_obj = dict()
-        text_obj['url'] = commit_comment['url']
-        text_obj['body'] = commit_comment['body']
-        text_obj['reactions'] = commit_comment['reactions']
-        text_obj['author_association'] = commit_comment['author_association']
-        user_data[commit_comment['user']['login']]['commit_comments'].append(text_obj)
+        if commit_comment['user'] is not None:
+            if user_data.get(commit_comment['user']['login'], None) is None:
+                init_user_data(user_data, commit_comment['user']['login'])
+            text_obj = dict()
+            text_obj['url'] = commit_comment['url']
+            text_obj['body'] = commit_comment['body']
+            text_obj['reactions'] = commit_comment['reactions']
+            text_obj['author_association'] = commit_comment['author_association']
+            user_data[commit_comment['user']['login']]['commit_comments'].append(text_obj)
 
     for review_comment in repo_data['review_comments']:
-        if user_data.get(review_comment['user']['login'], None) is None:
-            init_user_data(user_data, review_comment['user']['login'])
-        text_obj = dict()
-        text_obj['url'] = review_comment['url']
-        text_obj['body'] = review_comment['body']
-        text_obj['reactions'] = review_comment['reactions']
-        text_obj['author_association'] = review_comment['author_association']
-        user_data[review_comment['user']['login']]['review_comments'].append(text_obj)
+        if review_comment['user'] is not None:
+            if user_data.get(review_comment['user']['login'], None) is None:
+                init_user_data(user_data, review_comment['user']['login'])
+            text_obj = dict()
+            text_obj['url'] = review_comment['url']
+            text_obj['body'] = review_comment['body']
+            text_obj['reactions'] = review_comment['reactions']
+            text_obj['author_association'] = review_comment['author_association']
+            user_data[review_comment['user']['login']]['review_comments'].append(text_obj)
 
     # Save user data
     db.insert(user_data, 'data/user/%s.json' % full_repo_name)
