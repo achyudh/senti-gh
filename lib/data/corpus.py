c9a04b8403cb018872f9d9b132414dd2647a5b6f
Achyudh K Ram <achyudhk@gmail.com>
2018-03-15 17:53:22 -0400

    Add custom list corpus creation support

--- lib/data/corpus.py
+++ lib/data/corpus.py
@@ -1,8 +1,10 @@
-from lib.data import collate
+from lib.data import collate, download
+from lib.util import db
 import os.path
 
 
-def repo_reaper(dataset_path, num_pages=1, skip_existing=False, is_list=False):
+
+def repo_reaper(dataset_path, num_pages=10, skip_existing=False, is_list=False):
     with open(dataset_path, 'r') as dataset_csv:
         _first_line = dataset_csv.readline()
         for line in dataset_csv:
@@ -15,7 +17,43 @@ def repo_reaper(dataset_path, num_pages=1, skip_existing=False, is_list=False):
                 collate.by_user(full_repo_name, num_pages)
 
 
+def custom_annotated(dataset_path, full_repo_name, num_pages=10):
+    with open(dataset_path, 'r') as dataset_csv:
+        _first_line = dataset_csv.readline()
+        repo_data = dict()
+        for line in dataset_csv:
+            pr_data = dict()
+            pr_comments = list()
+            line_split = line.strip().split(',')
+            pr_number = int(line_split[0].split('/')[-1])
+            pr_info = download.pull_requests(full_repo_name, num_pages, pr_number=pr_number)[0]
+            pr_comments.append({"user": pr_info["user"], "body": pr_info["body"], "pr_body": True,
+                                "reactions": {"total_count": 0, "+1": 0, "-1": 0, "laugh": 0, "hooray": 0, "confused": 0, "heart": 0},})
+            pr_comments.extend(download.issue_comments(full_repo_name, num_pages, issue_number=pr_number))
+            if len(pr_comments) != int(line_split[2]):
+                print(line_split[0], len(pr_comments), line_split[2])
+            else:
+                ctr = 0
+                for pr_comment in pr_comments:
+                    pr_comment['ipa'] = line_split[1][ctr]
+                    ctr += 1
+            pr_data['comments'] = pr_comments
+            pr_data['info'] = pr_info
+            repo_data[pr_number] = pr_data
+    db.insert_generic(repo_data, dataset_path.split('.')[0] + ".json")
+
+
+def custom_list(repo_list, num_pages=10, skip_existing=False):
+    for full_repo_name in repo_list:
+        if not skip_existing or not(os.path.isfile('data/user/%s.json' % full_repo_name) and os.path.isfile('data/repo/%s.json' % full_repo_name)):
+            print("Fetching %s..." % full_repo_name)
+            collate.by_user(full_repo_name, num_pages)
+
+
+
 if __name__ == '__main__':
-    repo_reaper('data/repo_reaper/reaper_100.csv', num_pages=10, skip_existing=True, is_list=True)
+    custom_list(repo_list=["tensorflow/tensorflow", "onivim/oni", "spring-projects/spring-framework"])
+    # custom_annotated('data/labelled/pull_requests/tensorflow.csv', full_repo_name="tensorflow/tensorflow")
+    # repo_reaper('data/repo_reaper/reaper_100.csv', num_pages=10, skip_existing=True, is_list=True)
     # repo_reaper('data/repo_reaper/utility.csv', num_pages=10, skip_existing=True)
     # repo_reaper('data/repo_reaper/validation.csv', num_pages=10, skip_existing=True)
--- lib/data/download.py
+++ lib/data/download.py
@@ -30,7 +30,7 @@ def rate_reset_wait(headers):
         return "Positive RateLimit"
 
 
-def generic(request_url, headers=None, num_pages=1):
+def generic(request_url, headers=None, num_pages=1, iterable=True):
     merged_response = list()
     for i0 in range(num_pages):
         # print("Request:", request_url)
@@ -40,13 +40,19 @@ def generic(request_url, headers=None, num_pages=1):
             response = requests.get(request_url, auth=http_auth)
         wait_status = rate_reset_wait(response.headers)
         if wait_status == "Positive RateLimit":
-            merged_response.extend(response.json())
+            if iterable:
+                merged_response.extend(response.json())
+            else:
+                merged_response.append(response.json())
         else:
             if headers is not None:
                 response = requests.get(request_url, auth=http_auth, headers=headers)
             else:
                 response = requests.get(request_url, auth=http_auth)
-            merged_response.extend(response.json())
+            if iterable:
+                merged_response.extend(response.json())
+            else:
+                merged_response.append(response.json())
 
         # Change request_url to next url in the link
         if 'Link' in response.headers:
@@ -72,14 +78,22 @@ def commits(full_repo_name, num_pages=1):
     return generic(request_url, headers=reactions_header, num_pages=num_pages)
 
 
-def pull_requests(full_repo_name, num_pages=1):
-    request_url = 'https://api.github.com/repos/%s/pulls' % full_repo_name
-    return generic(request_url, headers=reactions_header, num_pages=num_pages)
+def pull_requests(full_repo_name, num_pages=1, pr_number=None):
+    if pr_number is None:
+        request_url = 'https://api.github.com/repos/%s/pulls' % full_repo_name
+        return generic(request_url, headers=reactions_header, num_pages=num_pages)
+    else:
+        request_url = 'https://api.github.com/repos/%s/pulls/%s' % (full_repo_name, pr_number)
+        return generic(request_url, headers=reactions_header, num_pages=num_pages, iterable=False)
 
 
-def issues(full_repo_name, num_pages=1):
-    request_url = 'https://api.github.com/repos/%s/issues' % full_repo_name
-    return generic(request_url, headers=reactions_header, num_pages=num_pages)
+def issues(full_repo_name, num_pages=1, issue_number=None):
+    if issue_number is None:
+        request_url = 'https://api.github.com/repos/%s/issues' % full_repo_name
+        return generic(request_url, headers=reactions_header, num_pages=num_pages)
+    else:
+        request_url = 'https://api.github.com/repos/%s/pulls/%s' % (full_repo_name, issue_number)
+        return generic(request_url, headers=reactions_header, num_pages=num_pages, iterable=False)
 
 
 def commit_comments(full_repo_name, num_pages=1, commit_ref=None):
@@ -87,8 +101,8 @@ def commit_comments(full_repo_name, num_pages=1, commit_ref=None):
         request_url = 'https://api.github.com/repos/%s/comments' % full_repo_name
         return generic(request_url, headers=reactions_header, num_pages=num_pages)
     else:
-        request_url = 'https://api.github.com/repos/%s/commits/%s/comments' % full_repo_name, commit_ref
-        return generic(request_url, headers=reactions_header)
+        request_url = 'https://api.github.com/repos/%s/commits/%s/comments' % (full_repo_name, commit_ref)
+        return generic(request_url, headers=reactions_header, iterable=False, num_pages=num_pages)
 
 
 def review_comments(full_repo_name, num_pages=1, pr_number=None):
@@ -96,8 +110,8 @@ def review_comments(full_repo_name, num_pages=1, pr_number=None):
         request_url = 'https://api.github.com/repos/%s/pulls/comments' % full_repo_name
         return generic(request_url, headers=reactions_header, num_pages=num_pages)
     else:
-        request_url = 'https://api.github.com/repos/%s/pulls/%s/comments' % full_repo_name, pr_number
-        return generic(request_url, headers=reactions_header)
+        request_url = 'https://api.github.com/repos/%s/pulls/%s/comments' % (full_repo_name, pr_number)
+        return generic(request_url, headers=reactions_header, num_pages=num_pages)
 
 
 def issue_comments(full_repo_name, num_pages=1, issue_number=None):
@@ -105,5 +119,5 @@ def issue_comments(full_repo_name, num_pages=1, issue_number=None):
         request_url = 'https://api.github.com/repos/%s/issues/comments' % full_repo_name
         return generic(request_url, headers=reactions_header, num_pages=num_pages)
     else:
-        request_url = 'https://api.github.com/repos/%s/issues/%s/comments' % full_repo_name, issue_number
-        return generic(request_url, headers=reactions_header)
+        request_url = 'https://api.github.com/repos/%s/issues/%s/comments' % (full_repo_name, issue_number)
+        return generic(request_url, headers=reactions_header, num_pages=num_pages)
