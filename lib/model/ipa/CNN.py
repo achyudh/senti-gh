74d65baece968d73349944658b296aa8c8bccc6c
Achyudh K Ram <achyudhk@gmail.com>
2018-04-05 22:35:24 -0400

    Add Early Stopping and other optmizations to IPA/CNN

--- lib/model/ipa/CNN.py
+++ lib/model/ipa/CNN.py
@@ -1,24 +1,20 @@
 from lib.embedding import word2vec, fasttext
 from sklearn.metrics import precision_recall_fscore_support, accuracy_score
 from sklearn.model_selection import StratifiedKFold
-from tensorflow.python.keras.layers import Dense, Input, Flatten, Embedding, Concatenate
+from tensorflow.python.keras.layers import Dense, Input, Embedding, Concatenate
 from tensorflow.python.keras.layers import Conv1D, MaxPooling1D, Dropout, GlobalMaxPool1D
 from tensorflow.python.keras.models import Model
+from tensorflow.python.keras.callbacks import EarlyStopping
 from tensorflow.python.keras.utils import to_categorical
 from tensorflow.python.keras.preprocessing.text import Tokenizer
 from tensorflow.python.keras.preprocessing.sequence import pad_sequences
-from gensim.models import phrases
-from nltk.tokenize import word_tokenize
 from tinydb import TinyDB
-from lib.data import fetch
-from lib.util import preprocessing, ngram
 import tensorflow as tf
-import pandas as pd
 import numpy as np
 
 
 def train_dual(train_x, train_y, evaluate_x, evaluate_y, embedding_map_1, embedding_map_2, embedding_dim_1, embedding_dim_2, max_sequence_len, num_classes):
-    with tf.device('/gpu:1'):
+    with tf.device('/gpu:0'):
         sequence_input = Input(shape=(max_sequence_len,), dtype='int32')
         l_embedding1 = Embedding(len(word_index) + 1, embedding_dim_1, weights=[embedding_map_1],
                                 input_length=max_sequence_len, trainable=False)
@@ -44,16 +40,17 @@ def train_dual(train_x, train_y, evaluate_x, evaluate_y, embedding_map_1, embedd
         l_dense3 = Dense(20, activation='relu')(l_dropout2)
         l_dropout3 = Dropout(0.2)(l_dense3)
         preds = Dense(num_classes, activation='softmax')(l_dropout3)
-
         cnn_model = Model(sequence_input, preds)
+        early_stopping_callback = EarlyStopping(patience=1)
         cnn_model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
         cnn_model.summary()
-        cnn_model.fit(train_x, train_y, validation_data=(evaluate_x, evaluate_y), epochs=8, batch_size=128)
+        cnn_model.fit(train_x, train_y, validation_data=(evaluate_x, evaluate_y), epochs=12, batch_size=64,
+                      callbacks=[early_stopping_callback])
     return cnn_model
 
 
 def train(train_x, train_y, evaluate_x, evaluate_y, embedding_map, embedding_dim, max_sequence_len, num_classes):
-    with tf.device('/gpu:1'):
+    with tf.device('/gpu:0'):
         embedding_layer_1 = Embedding(len(word_index) + 1, embedding_dim, weights=[embedding_map],
                                 input_length=max_sequence_len, trainable=False)
         sequence_input = Input(shape=(max_sequence_len,), dtype='int32')
@@ -62,15 +59,17 @@ def train(train_x, train_y, evaluate_x, evaluate_y, embedding_map, embedding_dim
         l_pool1 = MaxPooling1D(5)(l_conv1)
         l_conv2 = Conv1D(150, 5, activation='relu')(l_pool1)
         l_pool3 = GlobalMaxPool1D()(l_conv2)
-        l_dense1 = Dense(80, activation='relu')(l_pool3)
+        l_dense1 = Dense(100, activation='relu')(l_pool3)
         l_dropout1 = Dropout(0.2)(l_dense1)
-        l_dense2 = Dense(20, activation='relu')(l_dropout1)
+        l_dense2 = Dense(50, activation='relu')(l_dropout1)
         l_dropout2 = Dropout(0.2)(l_dense2)
-        preds = Dense(num_classes, activation='sigmoid')(l_dropout2)
+        preds = Dense(num_classes, activation='softmax')(l_dropout2)
         cnn_model = Model(sequence_input, preds)
+        early_stopping_callback = EarlyStopping(patience=1)
         cnn_model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
         cnn_model.summary()
-        cnn_model.fit(train_x, train_y, validation_data=(evaluate_x, evaluate_y), epochs=8, batch_size=128)
+        cnn_model.fit(train_x, train_y, validation_data=(evaluate_x, evaluate_y), epochs=12, batch_size=64,
+                      callbacks=[early_stopping_callback])
     return cnn_model
 
 
@@ -120,10 +119,10 @@ def cross_val(data_x, data_y, embedding_map, embedding_dim, max_sequence_len, nu
 
 
 if __name__ == '__main__':
-    embedding_dim_1 =  embedding_dim_2 = 300
-    num_classes = 4
+    embedding_dim_1 = embedding_dim_2 = 300
+    num_classes = 3
     dataset_list = ['data/labelled/pull_requests/spring.json', 'data/labelled/pull_requests/oni.json', 'data/labelled/pull_requests/tensorflow.json']
-    label_map = {"a": 0, "b": 1, "c": 2, "d": 3}
+    label_map = {"a": 0, "b": 1, "c": 2}
     data_xs = set()
     data_x = list()
     data_y = list()
@@ -132,7 +131,7 @@ if __name__ == '__main__':
         for entry in db:
             for thread in entry.values():
                 for comment in thread['comments']:
-                    if 'ipa' in comment and comment['ipa'] != '-':
+                    if 'ipa' in comment and (comment['ipa'] != '-'):
                         comment_body = comment['body'].lower()
                         if comment_body not in data_xs:
                             data_xs.add(comment_body)
@@ -140,16 +139,19 @@ if __name__ == '__main__':
                             data_y.append(label_map[comment['ipa'].lower()])
 
     data_x = np.array(data_x)
-    print(len(data_y), len(data_x))
+    print("Dataset loaded to memory. Size:", len(data_y))
     tokenizer = Tokenizer()
     tokenizer.fit_on_texts(data_x)
     sequences = tokenizer.texts_to_sequences(data_x)
-    max_sequence_len = max(len(seq) for seq in sequences)
-    max_sequence_len = min(500, max_sequence_len)
+    seq_lengths = [len(seq) for seq in sequences]
+    max_sequence_len = max(seq_lengths)
+    # avg_sequence_len = sum(seq_lengths)/len(seq_lengths)
+    # print(max_sequence_len, avg_sequence_len)
+    max_sequence_len = min(400, max_sequence_len)
     data_x = pad_sequences(sequences, maxlen=max_sequence_len)
     data_y_cat = to_categorical(data_y, num_classes=num_classes)
     word_index = tokenizer.word_index
-    embedding_map_1 = word2vec.embedding_matrix(word_index, model_path="data/embedding/word2vec/googlenews_size300.bin", binary=True)
+    # embedding_map_1 = word2vec.embedding_matrix(word_index, model_path="data/embedding/word2vec/googlenews_size300.bin", binary=True)
     embedding_map_2 = word2vec.embedding_matrix(word_index)
-    cross_val(data_x, data_y_cat, embedding_map_1, embedding_dim_1, max_sequence_len, num_classes, n_splits=10)
+    cross_val(data_x, data_y_cat, embedding_map_2, embedding_dim_2, max_sequence_len, num_classes, n_splits=10)
     # cross_val_dual(data_x, data_y_cat, embedding_map_1, embedding_map_2, embedding_dim_1, embedding_dim_2,max_sequence_len, num_classes, n_splits=10)
