b1660ed7a81404340b575a23c1bbaf7cf2a683a1
Achyudh K Ram <achyudhk@gmail.com>
2018-04-10 01:25:01 -0400

    Add Naive Bayes baseline for sentiment prediction

---
+++ lib/model/sentiments/NBC.py
@@ -0,0 +1,58 @@
+from sklearn.metrics import precision_recall_fscore_support, accuracy_score
+from sklearn.model_selection import StratifiedKFold
+from nltk.classify import NaiveBayesClassifier
+from nltk.sentiment import SentimentAnalyzer
+from nltk.sentiment.util import *
+import pandas as pd
+import numpy as np
+
+
+def train(train_x, train_y):
+    train_x = list(zip(train_x, train_y))
+    sentim_analyzer = SentimentAnalyzer()
+    all_words_neg = sentim_analyzer.all_words([mark_negation(doc) for doc in train_x])
+    unigram_feats = sentim_analyzer.unigram_word_feats(all_words_neg, min_freq=5)
+    sentim_analyzer.add_feat_extractor(extract_unigram_feats, unigrams=unigram_feats)
+    training_set = sentim_analyzer.apply_features(train_x)
+    trainer = NaiveBayesClassifier.train
+    classifier = sentim_analyzer.train(trainer, training_set)
+    return sentim_analyzer, classifier
+
+
+def predict(sentim_analyzer, classifier, predict_x):
+    test_set = sentim_analyzer.apply_features(predict_x, labeled=False)
+    predict_y = classifier.classify_many(test_set)
+    return predict_y
+
+
+def evaluate(sentim_analyzer, classifier, evaluate_x, evaluate_y):
+    predict_y = predict(sentim_analyzer, classifier, evaluate_x)
+    return {"individual": precision_recall_fscore_support(evaluate_y, predict_y),
+            "micro-average": precision_recall_fscore_support(evaluate_y, predict_y, average="micro")}
+
+
+def cross_val(data_x, data_y, num_classes, n_splits=5):
+    skf = StratifiedKFold(n_splits, random_state=157)
+    print("Performing cross validation (%d-fold)..." % n_splits)
+    precision_list = [0 for i in range(num_classes)]
+    recall_list = [0 for i in range(num_classes)]
+    mean_accuracy = 0
+    for train_index, test_index in skf.split(data_x, data_y):
+        sentim_analyzer, classifier = train(data_x[train_index], data_y[train_index])
+        metrics = evaluate(sentim_analyzer, classifier, data_x[test_index], data_y[test_index])
+        precision_list = [x + y for x, y in zip(metrics['individual'][0], precision_list)]
+        recall_list = [x + y for x, y in zip(metrics['individual'][1], recall_list)]
+        mean_accuracy += metrics['micro-average'][0]
+        print("Precision, Recall, F_Score, Support")
+        print(metrics)
+    print("Mean accuracy: %s Mean precision: %s, Mean recall: %s" % (mean_accuracy/n_splits, [precision/n_splits for precision in precision_list], [recall/n_splits for recall in recall_list]))
+
+
+if __name__ == '__main__':
+    num_classes = 2
+    data = pd.read_csv("data/labelled/JIRA.csv").as_matrix()
+    # data = pd.read_csv("data/labelled/StackOverflow.csv", encoding='latin1').as_matrix()
+    data_x = np.array([x.lower().split() for x in data[:,0]])
+    data_y = np.array([int(x) for x in data[:,1]])
+    print("Dataset loaded to memory. Size:", len(data_y))
+    cross_val(data_x, data_y, num_classes, n_splits=10)
