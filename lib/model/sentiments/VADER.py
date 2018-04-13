a8e272c996602842367e60fb54702182559c8735
Achyudh K Ram <achyudhk@gmail.com>
2018-04-13 03:49:09 -0400

    Add multiple dataset evaluation to VADER

--- lib/model/sentiments/VADER.py
+++ lib/model/sentiments/VADER.py
@@ -34,10 +34,48 @@ def evaluate(evaluate_x, evaluate_y, has_neutral=False):
             "micro-average": precision_recall_fscore_support(evaluate_y, predict_y, average="micro")}
 
 
+def evaluate_multiple(data_1, data_2, data_3, has_neutral=False):
+    precision_list = [0 for i in range(3 if has_neutral else 2)]
+    recall_list = [0 for i in range(3 if has_neutral else 2)]
+    mean_accuracy = 0
+    data_x1 = np.array([x for x in data_1[:,0]])
+    data_y1 = np.array([int(x) for x in data_1[:,1]])
+    data_x2 = np.array([x for x in data_2[:,0]])
+    data_y2 = np.array([int(x) for x in data_2[:,1]])
+    data_x3 = np.array([x for x in data_3[:,0]])
+    data_y3 = np.array([int(x) for x in data_3[:,1]])
+
+    metrics = evaluate(data_x1, data_y1, has_neutral=False)
+    mean_accuracy += metrics['micro-average'][0]
+    precision_list = [x + y for x, y in zip(metrics['individual'][0], precision_list)]
+    recall_list = [x + y for x, y in zip(metrics['individual'][1], recall_list)]
+    print("Accuracy: %s Precision: %s, Recall: %s" % (metrics['micro-average'][0], metrics['individual'][0], metrics['individual'][1]))
+
+    metrics = evaluate(data_x2, data_y2, has_neutral=False)
+    mean_accuracy += metrics['micro-average'][0]
+    precision_list = [x + y for x, y in zip(metrics['individual'][0], precision_list)]
+    recall_list = [x + y for x, y in zip(metrics['individual'][1], recall_list)]
+    print("Accuracy: %s Precision: %s, Recall: %s" % (metrics['micro-average'][0], metrics['individual'][0], metrics['individual'][1]))
+
+    metrics = evaluate(data_x3, data_y3, has_neutral=False)
+    mean_accuracy += metrics['micro-average'][0]
+    precision_list = [x + y for x, y in zip(metrics['individual'][0], precision_list)]
+    recall_list = [x + y for x, y in zip(metrics['individual'][1], recall_list)]
+    print("Accuracy: %s Precision: %s, Recall: %s" % (metrics['micro-average'][0], metrics['individual'][0], metrics['individual'][1]))
+
+    print("Mean accuracy: %s Mean precision: %s, Mean recall: %s" % (mean_accuracy/3, [precision/3 for precision in precision_list], [recall/3 for recall in recall_list]))
+
+
 if __name__ == '__main__':
-    data = pd.read_csv("data/labelled/Gerrit.csv").as_matrix()
-    # data = pd.read_csv("data/labelled/StackOverflow.csv", encoding='latin1').as_matrix()
-    data_x = np.array([x for x in data[:,0]])
-    data_y = np.array([int(x) for x in data[:,1]])
-    print("Dataset loaded to memory. Size:", len(data_y))
-    print(evaluate(data_x, data_y, has_neutral=False))
+    # data = pd.read_csv("data/labelled/Gerrit.csv").as_matrix()
+    # data = pd.read_csv("data/labelled/StackOverflowcsv", encoding='latin1').as_matrix()
+    data_1 = pd.read_csv("data/labelled/StackOverflow2.csv", encoding='latin1').as_matrix()
+    data_2 = pd.read_csv("data/labelled/Gerrit.csv").as_matrix()
+    data_3 = pd.read_csv("data/labelled/JIRA.csv").as_matrix()
+    evaluate_multiple(data_1, data_2, data_3)
+    # data = pd.concat([data_1, data_2, data_3]).as_matrix()
+    # data_x = np.array([x for x in data[:,0]])
+    # data_y = np.array([int(x) for x in data[:,1]])
+    # print("Dataset loaded to memory. Size:", len(data_y))
+    # metrics = evaluate(data_x, data_y, has_neutral=False)
+    # print("Mean accuracy: %s Mean precision: %s, Mean recall: %s" % (metrics['micro-average'][0], metrics['individual'][0], metrics['individual'][1]))
