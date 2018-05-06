aa22beb75f62035ef862e67c8b87768faf3ac23b
Achyudh K Ram <achyudhk@gmail.com>
2018-05-06 05:43:46 -0400

    Fix F1 output in SentiCR wrapper

--- lib/external/SentiCR/SentiCR.py
+++ lib/external/SentiCR/SentiCR.py
@@ -272,6 +272,7 @@ def cross_val(dataset, data_y, classifier, num_classes=2, n_splits=10):
     count = 1
     for train, test in skf.split(dataset, data_y):
         print("Iteration %d of %d" % (count, n_splits))
+        count += 1
         classifier_model = SentiCR(algo=classifier, training_data=dataset[train])
         test_comments = [comments.text for comments in dataset[test]]
         test_ratings = [comments.rating for comments in dataset[test]]
@@ -280,12 +281,12 @@ def cross_val(dataset, data_y, classifier, num_classes=2, n_splits=10):
                    "micro-average": precision_recall_fscore_support(test_ratings, pred, average="micro")}
         precision_list = [x + y for x, y in zip(metrics['individual'][0], precision_list)]
         recall_list = [x + y for x, y in zip(metrics['individual'][1], recall_list)]
+        f1_list = [x + y for x, y in zip(metrics['individual'][2], f1_list)]
         mean_accuracy += metrics['micro-average'][0]
-        count += 1
-        print("Precision, Recall, F_Score, Support")
-        print(metrics)
-
-    print("Mean accuracy: %s Mean precision: %s, Mean recall: %s" % (mean_accuracy/n_splits, [precision/n_splits for precision in precision_list], [recall/n_splits for recall in recall_list]))
+        print("Accuracy: %s, Precision: %s, Recall: %s, F1: %s" % (metrics['micro-average'][0], metrics['individual'][0],
+                                                                   metrics['individual'][1], metrics['individual'][2]))
+    print("Mean accuracy: %s Mean precision: %s, Mean recall: %s, Mean F1: %s" % (mean_accuracy/n_splits, [precision/n_splits for precision in precision_list],
+                                                                                  [recall/n_splits for recall in recall_list], [f1/n_splits for f1 in f1_list]))
     return mean_accuracy, precision_list, recall_list
 
 
