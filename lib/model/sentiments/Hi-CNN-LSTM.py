2a3a665d26b4204646a89b2fd4d787f3baaf0f6e
Achyudh K Ram <achyudhk@gmail.com>
2018-05-06 04:25:21 -0400

    Add F1 output to Hi-CNN-LSTM

--- lib/model/sentiments/Hi-CNN-LSTM.py
+++ lib/model/sentiments/Hi-CNN-LSTM.py
@@ -60,6 +60,7 @@ def cross_val(data_x, data_y, embedding_map, embedding_dim, max_sequence_len, ma
     print("Performing cross validation (%d-fold)..." % n_splits)
     precision_list = [0 for i in range(num_classes)]
     recall_list = [0 for i in range(num_classes)]
+    f1_list = [0 for i in range(num_classes)]
     mean_accuracy = 0
     iteration = 1
     for train_index, test_index in skf.split(data_x, data_y.argmax(axis=1)):
@@ -74,10 +75,14 @@ def cross_val(data_x, data_y, embedding_map, embedding_dim, max_sequence_len, ma
         print("Test time:", time.time() - test_start_time)
         precision_list = [x + y for x, y in zip(metrics['individual'][0], precision_list)]
         recall_list = [x + y for x, y in zip(metrics['individual'][1], recall_list)]
+        f1_list = [x + y for x, y in zip(metrics['individual'][2], f1_list)]
+        print("Accuracy: %s, Precision: %s, Recall: %s, F1: %s" % (metrics['micro-average'][0], metrics['individual'][0],
+                                                                   metrics['individual'][1], metrics['individual'][2]))
         mean_accuracy += metrics['micro-average'][0]
         print("Precision, Recall, F_Score, Support")
         print(metrics)
-    print("Mean accuracy: %s Mean precision: %s, Mean recall: %s" % (mean_accuracy/n_splits, [precision/n_splits for precision in precision_list], [recall/n_splits for recall in recall_list]))
+    print("Mean accuracy: %s Mean precision: %s, Mean recall: %s, Mean F1: %s" % (mean_accuracy/n_splits, [precision/n_splits for precision in precision_list],
+                                                                                 [recall/n_splits for recall in recall_list], [f1/n_splits for f1 in f1_list]))
 
 
 def bootstrap_trend(data_list, embedding_dim, num_classes):
@@ -112,7 +117,8 @@ def bootstrap_trend(data_list, embedding_dim, num_classes):
 def cross_dataset(data_list, embedding_map, embedding_dim, tokenizer, max_sequence_len, max_sequences, num_classes):
     precision_list = [0 for i in range(num_classes)]
     recall_list = [0 for i in range(num_classes)]
-    mean_accuracy = 0
+    f1_list = [0 for i in range(num_classes)]
+    accuracy_list = list()
 
     # Uncomment for resampling:
     # for i0 in range(len(data_list)):
@@ -127,12 +133,16 @@ def cross_dataset(data_list, embedding_map, embedding_dim, tokenizer, max_sequen
                 test_x, test_y_cat, _word_index, _max_sequence_len, _max_sequences = preprocessing.make_hierarchical_network_ready(data_test, num_classes, tokenizer, max_sequence_len, max_sequences, enforce_max_len=True)
                 cnn_pipeline = train(train_x, train_y_cat, test_x, test_y_cat, embedding_map, embedding_dim, tokenizer, max_sequence_len, max_sequences, num_classes, dataset_name)
                 metrics = evaluate(cnn_pipeline, test_x, test_y_cat)
-                mean_accuracy += metrics['micro-average'][0]
+                accuracy_list.append(metrics['micro-average'][0])
                 precision_list = [x + y for x, y in zip(metrics['individual'][0], precision_list)]
                 recall_list = [x + y for x, y in zip(metrics['individual'][1], recall_list)]
-                print(i0, i1, "Accuracy: %s Precision: %s, Recall: %s" % (metrics['micro-average'][0], metrics['individual'][0], metrics['individual'][1]))
-    print("Mean accuracy: %s Mean precision: %s, Mean recall: %s" % (mean_accuracy/len(mean_accuracy), [precision/len(mean_accuracy) for precision in precision_list],
-                                                                     [recall/len(mean_accuracy) for recall in recall_list]))
+                f1_list = [x + y for x, y in zip(metrics['individual'][2], f1_list)]
+                print(i0, i1, "Accuracy: %s, Precision: %s, Recall: %s, F1: %s" % (metrics['micro-average'][0], metrics['individual'][0],
+                                                                                   metrics['individual'][1], metrics['individual'][2]))
+    print("Mean accuracy: %s Mean precision: %s, Mean recall: %s, Mean F1: %s" % (sum(accuracy_list)/len(accuracy_list),
+                                                                                  [precision/len(accuracy_list) for precision in precision_list],
+                                                                                  [recall/len(accuracy_list) for recall in recall_list],
+                                                                                  [f1 / len(accuracy_list) for f1 in f1_list]))
 
 
 if __name__ == '__main__':
