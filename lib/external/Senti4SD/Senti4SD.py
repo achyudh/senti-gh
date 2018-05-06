f677dea943dc568cfd330ee685bd788195f4b82b
Achyudh K Ram <achyudhk@gmail.com>
2018-05-05 23:51:42 -0400

    Add exception handling to Senti4SD wrapper

--- lib/external/Senti4SD/Senti4SD.py
+++ lib/external/Senti4SD/Senti4SD.py
@@ -4,36 +4,42 @@ from sklearn.utils import resample
 import pandas as pd
 import numpy as np
 import subprocess
+import time
 
 
 def train(train_x, train_y):
     with open("input.csv", 'w', encoding='utf-8') as text_file:
         for line, label in zip(train_x, train_y):
             text_file.write(' '.join(line.split()) + ',' + str(label) + '\n')
-
-    subprocess.run(["java", "-jar", ".\\lib\\external\\Senti4SD\\NgramsExtraction.jar", "input.csv",
-                    "true"]).check_returncode()
-    subprocess.run(["java",  "-jar", "-Xmx5000m", ".\\lib\\external\\Senti4SD\\Senti4SD.jar", "-F", "A", "-i",
-                    "input.csv", "-W", ".\\lib\\external\\Senti4SD\\dsm.bin", "-oc", "output.csv", "-vd", "600",
-                    "-L", "-ul", "UnigramsList", "bl", "BigramsList"]).check_returncode()
-    subprocess.run(["Rscript", ".\\lib\\external\\Senti4SD\\parameterTuning.R", "temp",
-                    ".\\lib\\external\\Senti4SD\\modelsLiblinear", "output.csv"]).check_returncode()
-    subprocess.run(["Rscript", ".\\lib\\external\\Senti4SD\\trainModel.R", "temp",
-                    "temp\\L2-regularized_logistic_regression_(primal).txt", "output.csv"]).check_returncode()
+    try:
+        subprocess.run(["java", "-jar", "./lib/external/Senti4SD/NgramsExtraction.jar", "input.csv",
+                        "true"]).check_returncode()
+        subprocess.run(["java",  "-jar", "-Xmx5000m", "./lib/external/Senti4SD/Senti4SD.jar", "-F", "A", "-i",
+                        "input.csv", "-W", "./lib/external/Senti4SD/dsm.bin", "-oc", "output.csv", "-vd", "600",
+                        "-L", "-ul", "UnigramsList", "bl", "BigramsList"]).check_returncode()
+        subprocess.run(["Rscript", "./lib/external/Senti4SD/parameterTuning.R", "temp",
+                        "./lib/external/Senti4SD/modelsLiblinear", "output.csv"]).check_returncode()
+        subprocess.run(["Rscript", "./lib/external/Senti4SD/trainModel.R", "temp",
+                        "temp/L2-regularized_logistic_regression_(primal).txt", "output.csv"]).check_returncode()
+    except subprocess.CalledProcessError as e:
+        print(e.returncode)
+        print(e.output)
 
 
 def predict(predict_x):
     with open("test.txt", 'w', encoding='utf-8') as text_file:
         for line in predict_x:
             text_file.write(' '.join(line.split()) + '\n')
-
-    subprocess.run(["java", "-jar", ".\\lib\\external\\Senti4SD\\NgramsExtraction.jar", "test.txt"]).check_returncode()
-    subprocess.run(["java",  "-jar", "-Xmx5000m", ".\\lib\\external\\Senti4SD\\Senti4SD.jar", "-F", "A", "-i",
-                    "test.txt", "-W", ".\\lib\\external\\Senti4SD\\dsm.bin", "-oc", "output.csv", "-vd", "600",
-                    "-ul", "UnigramsList", "bl", "BigramsList"]).check_returncode()
-    subprocess.run(["Rscript", ".\\lib\\external\\Senti4SD\\classification.R", "output.csv", "predictions.csv",
-                    "temp\\modelLiblinear_L2-regularized_logistic_regression_(primal).Rda"]).check_returncode()
-
+    try:
+        subprocess.run(["java", "-jar", "./lib/external/Senti4SD/NgramsExtraction.jar", "test.txt"]).check_returncode()
+        subprocess.run(["java",  "-jar", "-Xmx5000m", "./lib/external/Senti4SD/Senti4SD.jar", "-F", "A", "-i",
+                        "test.txt", "-W", "./lib/external/Senti4SD/dsm.bin", "-oc", "output.csv", "-vd", "600",
+                        "-ul", "UnigramsList", "bl", "BigramsList"]).check_returncode()
+        subprocess.run(["Rscript", "./lib/external/Senti4SD/classification.R", "output.csv", "predictions.csv",
+                        "temp/modelLiblinear_L2-regularized_logistic_regression_(primal).Rda"]).check_returncode()
+    except subprocess.CalledProcessError as e:
+        print(e.returncode)
+        print(e.output)
     predict_y = list()
     with open("predictions.csv", 'r') as text_file:
         _temp = text_file.readline()
@@ -55,8 +61,12 @@ def cross_val(data_x, data_y, num_classes, n_splits=5):
     recall_list = [0 for i in range(num_classes)]
     mean_accuracy = 0
     for train_index, test_index in skf.split(data_x, data_y):
+        train_start_time = time.time()
         train(data_x[train_index], data_y[train_index])
+        print("Train time:", time.time() - train_start_time)
+        test_start_time = time.time()
         metrics = evaluate(data_x[test_index], data_y[test_index])
+        print("Test time:", time.time() - test_start_time)
         precision_list = [x + y for x, y in zip(metrics['individual'][0], precision_list)]
         recall_list = [x + y for x, y in zip(metrics['individual'][1], recall_list)]
         mean_accuracy += metrics['micro-average'][0]
@@ -66,7 +76,7 @@ def cross_val(data_x, data_y, num_classes, n_splits=5):
 
 
 def bootstrap_trend(data_x, data_y):
-    train_x, test_x, train_y, test_y = train_test_split(data_x, data_y, test_size=0.2, random_state=157)
+    train_x, test_x, train_y, test_y = train_test_split(data_x, data_y, test_size=0.3, random_state=157, stratify=True)
     print("Metrics: Precision, Recall, F_Score, Support")
     precision_list = [0 for i in range(num_classes)]
     recall_list = [0 for i in range(num_classes)]
@@ -128,71 +138,48 @@ def hard_cross_val(data_1, data_2, data_3, num_classes):
     print("Mean accuracy: %s Mean precision: %s, Mean recall: %s" % (mean_accuracy/3, [precision/3 for precision in precision_list], [recall/3 for recall in recall_list]))
 
 
-def evaluate_custom(data_1, data_2, data_3, num_classes):
-    data_x1 = np.array([x.lower().split() for x in data_1.as_matrix()[:,0]])
-    data_y1 = np.array([int(x) for x in data_1.as_matrix()[:,1]])
-    data_x2 = np.array([x.lower().split() for x in data_2.as_matrix()[:,0]])
-    data_y2 = np.array([int(x) for x in data_2.as_matrix()[:,1]])
-    data_x3 = np.array([x.lower().split() for x in data_3.as_matrix()[:,0]])
-    data_y3 = np.array([int(x) for x in data_3.as_matrix()[:,1]])
-    precision_list = [0 for i in range(num_classes)]
-    recall_list = [0 for i in range(num_classes)]
-    mean_accuracy = 0
-
-    train(data_x1, data_y1)
-    metrics = evaluate(data_x2, data_y2)
-    mean_accuracy += metrics['micro-average'][0]
-    precision_list = [x + y for x, y in zip(metrics['individual'][0], precision_list)]
-    recall_list = [x + y for x, y in zip(metrics['individual'][1], recall_list)]
-    print("Accuracy: %s Precision: %s, Recall: %s" % (metrics['micro-average'][0], metrics['individual'][0], metrics['individual'][1]))
-
-    metrics = evaluate(data_x3, data_y3)
-    mean_accuracy += metrics['micro-average'][0]
-    precision_list = [x + y for x, y in zip(metrics['individual'][0], precision_list)]
-    recall_list = [x + y for x, y in zip(metrics['individual'][1], recall_list)]
-    print("Accuracy: %s Precision: %s, Recall: %s" % (metrics['micro-average'][0], metrics['individual'][0], metrics['individual'][1]))
-
-    train(data_x2, data_y2)
-    metrics = evaluate(data_x1, data_y1)
-    mean_accuracy += metrics['micro-average'][0]
-    precision_list = [x + y for x, y in zip(metrics['individual'][0], precision_list)]
-    recall_list = [x + y for x, y in zip(metrics['individual'][1], recall_list)]
-    print("Accuracy: %s Precision: %s, Recall: %s" % (metrics['micro-average'][0], metrics['individual'][0], metrics['individual'][1]))
-
-    metrics = evaluate(data_x3, data_y3)
-    mean_accuracy += metrics['micro-average'][0]
-    precision_list = [x + y for x, y in zip(metrics['individual'][0], precision_list)]
-    recall_list = [x + y for x, y in zip(metrics['individual'][1], recall_list)]
-    print("Accuracy: %s Precision: %s, Recall: %s" % (metrics['micro-average'][0], metrics['individual'][0], metrics['individual'][1]))
-
-    train(data_x3, data_y3)
-    metrics = evaluate(data_x1, data_y1)
-    mean_accuracy += metrics['micro-average'][0]
-    precision_list = [x + y for x, y in zip(metrics['individual'][0], precision_list)]
-    recall_list = [x + y for x, y in zip(metrics['individual'][1], recall_list)]
-    print("Accuracy: %s Precision: %s, Recall: %s" % (metrics['micro-average'][0], metrics['individual'][0], metrics['individual'][1]))
-
-    metrics = evaluate(data_x2, data_y2)
-    mean_accuracy += metrics['micro-average'][0]
-    precision_list = [x + y for x, y in zip(metrics['individual'][0], precision_list)]
-    recall_list = [x + y for x, y in zip(metrics['individual'][1], recall_list)]
-    print("Accuracy: %s Precision: %s, Recall: %s" % (metrics['micro-average'][0], metrics['individual'][0], metrics['individual'][1]))
-
-    print("Mean accuracy: %s Mean precision: %s, Mean recall: %s" % (mean_accuracy/6, [precision/6 for precision in precision_list], [recall/6 for recall in recall_list]))
-
-
 if __name__ == '__main__':
     num_classes = 3
-    # data = pd.read_csv("data/labelled/Jira.csv").as_matrix()
-    data = pd.read_csv("data/labelled/StackOverflowSentiments.csv", encoding='latin1').as_matrix()
-    # data_1 = pd.read_csv("data/labelled/Gerrit.csv")
-    # data_2 = pd.read_csv("data/labelled/JIRA.csv")
-    # data_3 = pd.read_csv("data/labelled/StackOverflow2.csv", encoding='latin1')
+    # data = pd.read_csv("data/labelled/Gerrit.csv").as_matrix()
+    # data = pd.read_csv("data/labelled/StackOverflowJavaLibraries.csv", encoding='latin1').as_matrix()
+    data_1 = pd.read_csv("data/labelled/JIRA.csv")
+    data_2 = pd.read_csv("data/labelled/AppReviews.csv")
+    data_3 = pd.read_csv("data/labelled/Gerrit.csv")
+    data_4 = pd.read_csv("data/labelled/StackOverflowEmotions.csv", encoding='latin1')
+    data_5 = pd.read_csv("data/labelled/StackOverflowSentiments.csv", encoding='latin1')
+    data_6 = pd.read_csv("data/labelled/StackOverflowJavaLibraries.csv", encoding='latin1')
+    # data_list = [data_4, data_5, data_6]
     # hard_cross_val(data_1, data_2, data_3, num_classes)
     # evaluate_custom(data_1, data_2, data_3, num_classes)
     # data = pd.concat([data_1, data_2, data_3]).as_matrix()
+    # bootstrap_trend(data_x, data_y)
+    data = data_1.as_matrix()
     data_x = np.array([x.lower() for x in data[:,0]])
     data_y = np.array([int(x) for x in data[:,1]])
-    # print("Dataset loaded to memory. Size:", len(data_y))
+    print("Dataset loaded to memory. Size:", len(data_y))
     cross_val(data_x, data_y, num_classes, n_splits=10)
-    # bootstrap_trend(data_x, data_y)
+    data = data_2.as_matrix()
+    data_x = np.array([x.lower() for x in data[:,0]])
+    data_y = np.array([int(x) for x in data[:,1]])
+    print("Dataset loaded to memory. Size:", len(data_y))
+    cross_val(data_x, data_y, num_classes, n_splits=10)
+    data = data_3.as_matrix()
+    data_x = np.array([x.lower() for x in data[:,0]])
+    data_y = np.array([int(x) for x in data[:,1]])
+    print("Dataset loaded to memory. Size:", len(data_y))
+    cross_val(data_x, data_y, num_classes, n_splits=10)
+    data = data_4.as_matrix()
+    data_x = np.array([x.lower() for x in data[:,0]])
+    data_y = np.array([int(x) for x in data[:,1]])
+    print("Dataset loaded to memory. Size:", len(data_y))
+    cross_val(data_x, data_y, num_classes, n_splits=10)
+    data = data_5.as_matrix()
+    data_x = np.array([x.lower() for x in data[:,0]])
+    data_y = np.array([int(x) for x in data[:,1]])
+    print("Dataset loaded to memory. Size:", len(data_y))
+    cross_val(data_x, data_y, num_classes, n_splits=10)
+    data = data_6.as_matrix()
+    data_x = np.array([x.lower() for x in data[:,0]])
+    data_y = np.array([int(x) for x in data[:,1]])
+    print("Dataset loaded to memory. Size:", len(data_y))
+    cross_val(data_x, data_y, num_classes, n_splits=10)
